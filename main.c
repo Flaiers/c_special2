46420905e36baf82c6b771540711988579d12d10
Max <>
2021-04-25 17:31:25 +0000

    edits

--- main.c
+++ main.c
@@ -5,17 +5,14 @@
 int SumRequiredSymbols(char *str)
 {
   int signs = 0, digits = 0, sum;
-  char plus, minus, multiply;
-  plus = 43;
-  minus = 45;
-  multiply = 42;
-  
+  char plus = 43, minus = 45, multiply = 42;
+
   for (int s = 0; str[s] != '\0'; s++)
   {
     if (str[s] == plus || str[s] == minus || str[s] == multiply)
       signs++;
 
-    if ('0' <= str[s] && str[s] <= '9')
+    if (48 <= str[s] && str[s] <= 57)
       digits++;
   }
   sum = signs + digits;
@@ -24,10 +21,10 @@ int SumRequiredSymbols(char *str)
 
 int main(void)
 {
-  char str[64];
-  char out;
-  printf("Введите строку: ");
-  fgets(str, 64, stdin);
-  out = SumRequiredSymbols(str);
-  printf("Количество: %d\n", out);
+  char str[32];
+  char quantity;
+  printf("Enter a string: ");
+  fgets(str, 32, stdin);
+  quantity = SumRequiredSymbols(str);
+  printf("Quantity digits and signs '+', '-', '*': %d\n", quantity);
 }
